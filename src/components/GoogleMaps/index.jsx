79d02e3a97a5707df077f473272d236d27921295
dzianismakeichyk <dzianis.makeichyk@gmail.com>
2018-07-08 21:25:51 +0200

    add on offer

--- src/components/GoogleMaps/index.jsx
+++ src/components/GoogleMaps/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
+import GoogleMapApiKey from '../../keys/apiKeys';
 import './style.css'
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
@@ -10,22 +11,37 @@ class SimpleMap extends Component {
             lat: 59.95,
             lng: 30.33
         },
-        zoom: 11
+        zoom: 11,
+        markers: [
+            {lat: 59.95, lng: 30.33, img_src: 'YOUR-IMG-SRC'},
+            {lat: 59.85, lng: 30.23, img_src: 'YOUR-IMG-SRC' },
+            {lat: 59.75, lng: 30.13,  img_src: 'YOUR-IMG-SRC'}
+        ]
     };
 
     render() {
+        const props = this.props;
+        const ApiKey = GoogleMapApiKey.GoogleMapApiKey;
+
+        console.log(ApiKey)
+
         return (
             <div className="googleMap">
                 <GoogleMapReact
-                    bootstrapURLKeys={{ key: 'AIzaSyC6IBlaI46HTK5sIftIe3sVX3R-IL_I3h4'}}
-                    defaultCenter={this.props.center}
-                    defaultZoom={this.props.zoom}
+                    bootstrapURLKeys={{ key: ApiKey}}
+                    defaultCenter={props.center}
+                    defaultZoom={props.zoom}
                 >
-                    <AnyReactComponent
-                        lat={59.955413}
-                        lng={30.337844}
-                        text={'Kreyser Avrora'}
-                    />
+                    {props.markers.map((marker, i) =>{
+                        return(
+                            <AnyReactComponent
+                                key={i}
+                                lat={marker.lat}
+                                lng={marker.lng}
+                            />
+
+                        )
+                    })}
                 </GoogleMapReact>
             </div>
         );
---
+++ src/keys/apiKeys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    "GoogleMapApiKey": 'AIzaSyC6IBlaI46HTK5sIftIe3sVX3R-IL_I3h4'
+};
